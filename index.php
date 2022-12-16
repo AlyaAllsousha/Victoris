cd29b37758d77e8330629fa81d36f6adc977bf77
AlyaAllsousha <albisharafikova@yandex.ru>
2022-12-16 16:58:14 +0300

    чсмит

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <div class="container">
+            <div class="header">
+                <div class="logo">
+                    <img src="img/Group3.png" alt="sd">
+                </div>
+                <a href="#" class="btn">
+                    Вход
+                </a>
+        </div>
+        </div>
+    </section>
+</body>
+</html>
