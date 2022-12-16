d3a9ceb6e794cb969ad67f9f7f1943120d90e307
AlyaAllsousha <albisharafikova@yandex.ru>
2022-12-16 16:58:50 +0300

    орпаепно

--- index.php
+++ index.php
@@ -11,11 +11,27 @@
     <section>
         <div class="container">
             <div class="header">
+                <div class="header_inner">
                 <div class="logo">
                     <img src="img/Group3.png" alt="sd">
                 </div>
+                <div class="btn_wrap">
+                <a href="#" class="btn wh">
+                    Регистрация
+                </a>
                 <a href="#" class="btn">
                     Вход
+                </a>
+                </div> <!--btn_wrap-->
+                </div><!--header_inner-->
+            </div>
+            <div class="main">
+                <h1 class="main_text"><span class="main_orange">Создавай</span>,  играй,  обучайся!</h1>
+                <a href="" class="btn btn_main">
+                    Создать
+                </a>
+                <a href="" class="btn wh btn_main">
+                    Учиться
                 </a>
         </div>
         </div>
